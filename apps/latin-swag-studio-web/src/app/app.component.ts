5fd6f20f530e8e166c93e782d3c926ad30a40d3f
Josbell Quiros <josbell.quiros@oasisdigital.com>
2020-03-28 20:59:04 -0400

    initial commit

---
+++ apps/latin-swag-studio-web/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'swagex-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'latin-swag-studio-web';
+}
